b38b7393f549932ffa883d6e0ee5d91a8e125e17
Diego Roque <diegojoroque@gmail.com>
2022-07-15 19:59:38 -0300

    Desafios Propostos - Nível 1

--- Desafio Proposto 4 - Nivel 1.cpp
+++ Desafio Proposto 4 - Nivel 1.cpp
@@ -5,4 +5,34 @@ Módulo 1 - O começo da jornada!
 Disponível em https://www.youtube.com/watch?v=KcRW1rSC5nk e https://drive.google.com/file/d/1viOzA_458Cc5ibxaFcmCNiFViwcQUzGS/view
 
 Desafio 4
+
+Crie um Algoritmo em C++, que calcula a potência, em que o usuário informa um número real e um número natural. Nesse algoritmo, sempre que é informado o resultado da potência, é perguntado ao usuário, se este deseja calcular outra potência.
 */
+
+#include <iostream>
+
+using namespace std;
+
+float potencia (float x, int y){
+    float pot=1;
+    int cont=1;
+    while (cont <= y){
+        pot*=x; //identico a escrever pot=pot*x;
+        cont++; //identico a escrever cont=cont+1;
+    }
+    return pot;
+}
+
+int main(){
+    float b;
+    int e;
+    cout << "Digite a base (numero real):\n";
+    cin >> b;
+    cout << "Digite o expoente (numero natura):\n";
+    cin >> e;
+
+    float respost=potencia(b,e);
+    cout << "O resultado de " << b << " elevado a " << e << " e igual a " << respost << endl;
+
+    return 0;
+}
