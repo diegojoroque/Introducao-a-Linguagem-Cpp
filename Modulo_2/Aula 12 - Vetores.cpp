d29ddcbfa58b2336080916080ce97f15df0ae274
Diego Roque <diegojoroque@gmail.com>
2022-07-26 20:58:34 -0300

    Modulo_2

---
+++ Modulo_2/Aula 12 - Vetores.cpp
@@ -0,0 +1,14 @@
+/*
+Módulo 2 - Ponteiros, Vetores e Matrizes
+Aula 12 - Vetores
+Disponível em https://www.youtube.com/watch?v=aNNqxVeEv-k
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+    return 0;
+}
---
+++ Modulo_2/Aula 13 - Matrizes.cpp
@@ -0,0 +1,14 @@
+/*
+Módulo 2 - Ponteiros, Vetores e Matrizes
+Aula 13 - Matrizes
+Disponível em https://www.youtube.com/watch?v=rXiCpNlNlQI
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+    return 0;
+}
